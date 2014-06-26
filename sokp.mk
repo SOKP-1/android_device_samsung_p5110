5062be5fa2677bd686193ef924a460ed7d5d3e58
VivekNeel <vivek2neel@gmail.com>
2014-06-26 20:34:34 +0530

    Update and rename cm.mk to sokp.mk

--- sokp.mk
+++ sokp.mk
@@ -15,7 +15,7 @@
 #
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/sokp/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := p5110
@@ -24,15 +24,15 @@ PRODUCT_RELEASE_NAME := p5110
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 1280
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common SOKP stuff.
+$(call inherit-product, vendor/sokp/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/p5110/full_p5110.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p5110
-PRODUCT_NAME := cm_p5110
+PRODUCT_NAME := sokp_p5110
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-P5110
 PRODUCT_MANUFACTURER := samsung
